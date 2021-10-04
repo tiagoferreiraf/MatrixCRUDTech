793253f4c8ed1f31250e61cac970ea7f91ebdaf7
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-10-04 17:29:55 -0300

    Add project files.

---
+++ MatrixCRUDTech/Migrations/20211004183429_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MatrixCRUDTech.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(250)", nullable: false),
+                    EmpCode = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Position = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    OfficeLocation = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ MatrixCRUDTech/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MatrixCRUDTech.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Column(TypeName="nvarchar(250)")]
+        [Required(ErrorMessage = "Este campo é de preenchimento obrigatório")]
+        [DisplayName("Nome")]
+        public string FullName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [DisplayName("Matricula")]
+        public int EmpCode { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Cargo")]
+        public int Position { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Localização")]
+        public int OfficeLocation { get; set; }
+    }
+}
